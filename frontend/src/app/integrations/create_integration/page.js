7b1ec29b61e16d6aa63d5359961cf09f9b7347be
Tim Möhrke <t.moehrke@chi-deutschland.com>
2024-10-06 00:37:21 +0300

    FEAT: Add some more awesome stuff... help me.. im tired

--- frontend/src/app/integrations/create_integration/page.js
+++ frontend/src/app/integrations/create_integration/page.js
@@ -32,13 +32,11 @@ const CreateIntegrationPage = () => {
                 body: formData,
             });
 
-            console.log('Request completed with status:', response.status);
             const data = await response.json();
             setSchema(data);
-            console.log(data);
             setTimeout(() => {
               router.push('/integrations/confirm_schema');
-            }, 2000);
+            }, 1000);
         } catch (error) {
             console.error('Error:', error);
         }
@@ -53,41 +51,51 @@ const CreateIntegrationPage = () => {
   });
 
   return (
-    <div className="flex justify-center items-center min-h-screen position-relative">
-      <div className="flex flex-col items-center gap-5">
-        <h1 className="mt-16 mb-8 text-4xl">Mr. Bond, I've been awaiting your integration creation</h1>
-        <div className="bg-gray-100 max-w-screen-xl w-full p-5 custom-dropzone-container rounded-lg">
-          <div className="flex flex-col items-center gap-4 custom-dropzone-container">
-            <div className="w-full max-w-md">
-              <Input id="name" name="name" type="text" placeholder="Name of Integration" className="mt-1 block w-full bg-white text-gray-800" />
-            </div>
-            <div
-              {...getRootProps()}
-              className={`flex justify-center items-center h-96 bg-gray-200 w-full border-2 border-dashed ${
-                isDragActive ? 'border-blue-500' : 'border-gray-300 custom-dropzone'
-              }`}
-            >
-              <input {...getInputProps()} />
-              {isDragActive ? (
-                <p>Drop the files here ...</p>
-              ) : (
-                <p>Drag 'n' drop some files here, or click to select files (only .xls)</p>
-              )}
-            </div>
-            <button
-              onClick={handleUpload}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-            >
-              Upload File
-            </button>
-          </div>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-center mt-16">
+        <div className="flex items-center">
+          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-green-600 text-xl">1</div>
+          <div className="w-32 h-1 bg-gray-300 mx-2"></div>
+          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-300 text-xl">2</div>
+          <div className="w-32 h-1 bg-gray-300 mx-2"></div>
+          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-300 text-xl">3</div>
         </div>
       </div>
-      {isLoading && (
-        <div className="absolute inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
-          <span className="loader"></span>
+      <div className="flex justify-center items-center min-h-screen position-relative">
+        <div className="flex flex-col items-center gap-5">
+          <div className="bg-gray-100 w-[900px] p-5 custom-dropzone-container rounded-lg">
+            <div className="flex flex-col items-center gap-4 custom-dropzone-container">
+              <div className="w-full max-w-md">
+                <Input id="name" name="name" type="text" placeholder="Name of Integration" className="mt-1 block w-full bg-white text-gray-800" />
+              </div>
+              <div
+                {...getRootProps()}
+                className={`flex justify-center items-center h-96 bg-gray-200 w-full border-2 border-dashed ${
+                  isDragActive ? 'border-blue-500' : 'border-gray-300 custom-dropzone'
+                }`}
+              >
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <p>Drop the files here ...</p>
+                ) : (
+                  <p>Place your file here (only .xls)</p>
+                )}
+              </div>
+              <button
+                onClick={handleUpload}
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+              >
+                Upload File
+              </button>
+            </div>
+          </div>
         </div>
-      )}
+        {isLoading && (
+          <div className="absolute inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
+            <span className="loader"></span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
