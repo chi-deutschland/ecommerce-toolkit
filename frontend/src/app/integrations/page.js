98c2d8925454f44b2c312cff82a1c0b748844928
Tim Möhrke <t.moehrke@chi-deutschland.com>
2024-10-06 00:36:05 +0300

    FEAT:update integrations page

--- frontend/src/app/integrations/page.js
+++ frontend/src/app/integrations/page.js
@@ -2,26 +2,27 @@
 
 import { useAuth } from '@/context/AuthContext';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { BarChart, Bar, CartesianGrid, XAxis } from 'recharts';
 import * as React from "react";
 import { Label, Pie, PieChart } from "recharts";
-import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
+import { GlobalContext } from '@/context/GlobalContext';
 
 const integrationsData = [
     {
-      name: "Temu Fra",
+      name: "Temu LAX",
       createdAt: "2023-01-01",
       lastUpdate: "2023-02-01",
-      totalTonnage: 500000,
+      totalTonnage: 1254,
       barChartData: [
-        { month: 'Jan', shipments: 20000, tonnage: 100000 },
-        { month: 'Feb', shipments: 25000, tonnage: 120000 },
-        { month: 'Mar', shipments: 30000, tonnage: 150000 },
-        { month: 'Apr', shipments: 35000, tonnage: 175000 },
-        { month: 'May', shipments: 40000, tonnage: 200000 },
-        { month: 'Jun', shipments: 45000, tonnage: 225000 },
+        { month: 'Jan', shipments: 20000, tonnage: 223 },
+        { month: 'Feb', shipments: 25000, tonnage: 230 },
+        { month: 'Mar', shipments: 30000, tonnage: 100 },
+        { month: 'Apr', shipments: 35000, tonnage: 220 },
+        { month: 'May', shipments: 40000, tonnage: 22 },
+        { month: 'Jun', shipments: 45000, tonnage: 22},
       ],
       pieChartData: [
         { type: "cleared", shipments: 27500, fill: "var(--color-cleared)" },
@@ -29,26 +30,26 @@ const integrationsData = [
       ],
     },
     {
-      name: "Logi Corp",
+      name: "Shaoke AMS",
       createdAt: "2023-03-01",
       lastUpdate: "2023-04-01",
       totalShipments: 200000,
-      totalTonnage: 1000000,
+      totalTonnage: 9343,
       barChartData: [
         { month: 'Jan', shipments: 50000, tonnage: 250000 },
         { month: 'Feb', shipments: 60000, tonnage: 300000 },
         { month: 'Mar', shipments: 70000, tonnage: 350000 },
       ],
       pieChartData: [
-        { type: "cleared", shipments: 29000, fill: "var(--color-cleared)" },
+        { type: "cleared", shipments: 89000, fill: "var(--color-cleared)" },
         { type: "blocked", shipments: 20, fill: "var(--color-blocked)" },
       ],
     },
     {
-      name: "another card",
+      name: "Shein IST",
       createdAt: "2023-05-01",
       lastUpdate: "2023-06-01",
-      totalTonnage: 750000,
+      totalTonnage: 7532,
       barChartData: [
         { month: 'Jan', shipments: 40000, tonnage: 200000 },
         { month: 'Feb', shipments: 45000, tonnage: 220000 },
@@ -59,21 +60,21 @@ const integrationsData = [
         { type: "blocked", shipments: 200, fill: "var(--color-blocked)" },
       ],
     },
-    {
-      name: "heyo",
-      createdAt: "2023-05-01",
-      lastUpdate: "2023-06-01",
-      totalTonnage: 750000,
-      barChartData: [
-        { month: 'Jan', shipments: 40000, tonnage: 200000 },
-        { month: 'Feb', shipments: 45000, tonnage: 220000 },
-        { month: 'Mar', shipments: 50000, tonnage: 250000 },
-      ],
-      pieChartData: [
-        { type: "cleared", shipments: 27225, fill: "var(--color-cleared)" },
-        { type: "blocked", shipments: 200, fill: "var(--color-blocked)" },
-      ],
-    },
+    // {
+    //   name: "Temu FRA",
+    //   createdAt: "2024-05-01",
+    //   lastUpdate: "2024-10-05",
+    //   totalTonnage: 2324,
+    //   barChartData: [
+    //     { month: 'May', shipments: 20000, tonnage: 100000 },
+    //     { month: 'Aug', shipments: 25000, tonnage: 120000 },
+    //     { month: 'Oct', shipments: 30000, tonnage: 150000 },
+    //   ],
+    //   pieChartData: [
+    //     { type: "cleared", shipments: 27225, fill: "var(--color-cleared)" },
+    //     { type: "blocked", shipments: 200, fill: "var(--color-blocked)" },
+    //   ],
+    // },
   ];
 
 const chartConfig = {
@@ -125,6 +126,7 @@ function PieChartComponent(data) {
                       y={viewBox.cy}
                       textAnchor="middle"
                       dominantBaseline="middle"
+                       fill="#f0f0f0"
                     >
                       {totalshipments}
                     </text>
@@ -142,10 +144,16 @@ function PieChartComponent(data) {
 export default function Integrations() {
   const { isLoggedIn } = useAuth();
   const router = useRouter();
+  const { username } = useContext(GlobalContext);
+
 
   const handleAddNewIntegrationClick = () => {
     router.push('/integrations/create_integration');
   };
+
+  const routeToIntegration = () => {
+    router.push('/integrations/integration');
+  }
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -160,11 +168,11 @@ export default function Integrations() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="flex flex-col items-center gap-5">
-        <h1 className="mt-16 mb-8 text-4xl">Welcome James Bond</h1>
+        <h1 className="mt-16 mb-8 text-4xl">Welcome {username}</h1>
         <div className="container p-5 rounded-lg inline-block rounded-lg">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {integrationsData.map((integration, index) => (
-              <Card key={index} className="w-[370px] h-[370px] cursor-pointer hover:shadow-lg transition-shadow duration-300 rounded-lg">
+              <Card key={index} className="w-[370px] h-[370px] cursor-pointer hover:shadow-lg transition-shadow duration-300 rounded-lg" onClick={routeToIntegration}>
                 <CardHeader>
                   <CardTitle className="text-2xl">{integration.name}</CardTitle>
                 </CardHeader>
