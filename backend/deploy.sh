98b23bca85ee7880c7a46cec7bae66abefa900e1
Victor Wolf <v.wolf@chi-deutschland.com>
2024-10-06 09:19:16 +0300

    add GO OS and ARCH envs

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,1 +1,1 @@
-go build main.go && gcloud compute scp main instance-20241005-084327:~/main --zone "europe-west3-a" --project "data-integration-development"
+env GOOS=linux GOARCH=amd64 go build main.go && gcloud compute scp main instance-20241005-084327:~/main --zone "europe-west3-a" --project "data-integration-development"
